188e0100df10dac9d81f2e0d4cd779f3395c4f79
Thiago Vazquez <thiagomv@udel.edu>
2023-10-11 02:16:41 -0400

    Solved State Task

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,44 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(5);
+    const [rightDie, setRightDie] = useState<number>(6);
+    const [gameResult, setGameResult] = useState<string>("");
+    const rollLeftDie = () => {
+        const newValue = d6();
+        setLeftDie(newValue);
+        checkGameResult(newValue, rightDie);
+    };
+    const rollRightDie = () => {
+        const newValue = d6();
+        setRightDie(newValue);
+        checkGameResult(leftDie, newValue);
+    };
+    const checkGameResult = (leftValue: number, rightValue: number) => {
+        if (leftValue === 1 && rightValue === 1) {
+            setGameResult("You lose! Snake Eyes!");
+        } else if (leftValue === rightValue) {
+            setGameResult("You win! Matching values!");
+        } else {
+            setGameResult("");
+        }
+    };
+    return (
+        <div>
+            <h1>Two Dice Game</h1>
+            <div>
+                <span data-testid="leftDie"> {leftDie} </span>
+                <Button onClick={rollLeftDie} data-testid="roll-left">
+                    Roll Left
+                </Button>
+            </div>
+            <div>
+                <span data-testid="rightDie">{rightDie}</span>
+                <Button onClick={rollRightDie} data-testid="roll-right">
+                    Roll Right
+                </Button>
+            </div>
+            <p>{gameResult}</p>
+        </div>
+    );
 }
