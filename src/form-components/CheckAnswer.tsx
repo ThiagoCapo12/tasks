aae4bb6054bd48ec951d711f68841d2abde67428
Thiago Vazquez <thiagomv@udel.edu>
2023-10-22 18:20:10 -0400

    Solved Forms

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,27 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(event.target.value);
+    };
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <label htmlFor="userAnswer">Your Answer:</label>
+            <input
+                type="text"
+                id="userAnswer"
+                value={userAnswer}
+                onChange={handleChange}
+            />
+            {userAnswer === expectedAnswer ? (
+                <span>✔️ Correct!</span>
+            ) : (
+                <span>❌ Incorrect!</span>
+            )}
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedOption, setSelectedoption] = useState<string>(options[0]);
+    const isCorrect = selectedOption === expectedAnswer;
+    const handleOptionChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setSelectedoption(event.target.value);
+    };
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select value={selectedOption} onChange={handleOptionChange}>
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <div>
+                {isCorrect ? "✔️" : "❌"}
+                {isCorrect ? "Correct" : "Incorrect"}
+            </div>
         </div>
     );
 }
